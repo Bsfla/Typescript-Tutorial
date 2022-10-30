1e04dd793fbedf8add0cac2d45da157ff128916d
baebumsoo <baebeomsu77@gmail.com>
2022-10-30 23:51:51 +0900

    commit

--- typescript-allinone/chapter13.ts
+++ typescript-allinone/chapter13.ts
@@ -11,7 +11,8 @@ const zeroCho: P<Profile> ={
     name: 'zeroCho',
     age: 29
 }
-
+*/
+/*
 type P<T> = {
     [k in keyof T]? : T[k]
 }
@@ -21,13 +22,28 @@ type P<T, S extends keyof T> = {
   [k in S]: T[k];
 };
 
+type O<T, S extends keyof any> = Pick<T, Exclude<keyof T, S>>;
+
 interface Profile {
   name: string;
   age: number;
   married: boolean;
 }
 
-const zeroCho: P<Profile, "name" | "age"> = {
-  name: "zerocho",
-  age: 29,
+const zeroCho: Omit<Profile, "name"> = {
+  age: 12,
+  married: false,
 };
+
+type E<T, U> = T extends U ? never : U;
+
+type Animal1 = "Cat" | "Dog" | "Human";
+type Mamal = E<Animal1, "Human">;
+
+type R<T extends keyof any, S> = {
+  [key in T]: S;
+};
+
+const test: R<string, number> = { a: 3, b: 5, c: 7 };
+
+type A0 = string | null | undefined | boolean | number;
