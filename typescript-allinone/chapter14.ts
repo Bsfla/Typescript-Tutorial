7d1df5ad1d1e9a5a82f5896d50cdeb419c53b2f4
baebumsoo <baebeomsu77@gmail.com>
2022-12-24 23:26:43 +0900

    comit

--- typescript-allinone/chapter14.ts
+++ typescript-allinone/chapter14.ts
@@ -1,7 +1,15 @@
-function zip(x: number, y: string, z: boolean) : {x: number, y: string, z: boolean} {
-    return {x, y, z}
+function zip(
+  x: number,
+  y: string,
+  z: boolean
+): { x: number; y: string; z: boolean } {
+  return { x, y, z };
 }
 
-type Pa<T extends (...args: any) => any> =  T extends (...args: infer R) => any ? R : never 
+type Pa<T extends (...args: any) => any> = T extends (...args: infer R) => any
+  ? R
+  : never;
 
-type Params = Pa<typeof zip>
+type Params = Pa<typeof zip>;
+
+type = a;
